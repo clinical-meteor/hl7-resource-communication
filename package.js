9ffa21da48e95eccce0af66de5325909732d5daf
Abigail Watson <abigailwatson@Abigails-Mac-mini-379101.local>
2018-09-01 11:59:34 -0500

    simpl-schema updates; hipaa-logger support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-communication',
-  version: '0.0.7',
+  version: '0.1.0',
   summary: 'HL7 FHIR Resource - Communication',
   git: 'https://github.com/dpdonohue/hl7-resource-communication',
   documentation: 'README.md'
@@ -12,7 +12,11 @@ Package.onUse(function (api) {
 
   api.use('meteor-platform');
   api.use('mongo');
-  api.use('aldeed:simple-schema@1.3.3');
+
+  api.use('aldeed:collection2@3.0.0');
+  api.use('clinical:hl7-resource-datatypes@4.0.0');
+  api.use('clinical:hl7-resource-bundle@1.4.0');
+
   api.use('simple:json-routes@2.1.0');
   api.use('momentjs:moment@2.17.1');
   api.use('ecmascript@0.9.0');
@@ -21,10 +25,8 @@ Package.onUse(function (api) {
   api.use('react-meteor-data@0.2.15');
 
   api.use('clinical:extended-api@2.2.2');
-  api.use('clinical:base-model@1.3.5');
+  api.use('clinical:base-model@1.4.0');
   api.use('clinical:user-model@1.5.0');
-  api.use('clinical:hl7-resource-datatypes@3.0.0');
-  api.use('clinical:hl7-resource-bundle@1.3.10');
   api.use('matb33:collection-hooks@0.7.15');
   api.use('clinical:alert@2.1.3')
 
@@ -50,7 +52,8 @@ Package.onUse(function (api) {
 
 Npm.depends({
   "material-ui": "0.20.0",
-  "lodash": "4.17.4"
+  "lodash": "4.17.4",
+  "simpl-schema": "1.5.3"
 });
 
 
